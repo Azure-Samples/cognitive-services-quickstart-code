710f8d342c51c980cd11800f30da7d9ddaf4293f
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-08-08 21:52:31 -0700

    Adding Human Reviews example

--- java/ContentModerator/src/main/java/ContentModeratorQuickstart.java
+++ java/ContentModerator/src/main/java/ContentModeratorQuickstart.java
@@ -10,21 +10,29 @@ import java.util.concurrent.*;
 // </snippet_imports>
 
 /** \
- * This example reads two URL images from file, then moderates them. A successful response shows
- * a JSON representation of the moderation results.
+ * This quickstart:
+ *  - Moderates a URL image
+ *  - Moderates a text file
+ *  - Creates a human review for an image that posts to the Content Moderator website
+ * 
+ * A successful response shows a JSON representation of the moderation results.
  * 
  * Prerequisites:
  * 1. Obtain an Azure Content Moderator resource 
  * 2. Clone or download this repo: https://github.com/Azure-Samples/cognitive-services-java-sdk-samples.git
  * 3. Use Java 8 or later
  * 4. Set and add your Content Moderator subscription key and endpoint environment variables in main() below.
+ *    Aquire the endpoint from your Content Moderator account... 
+ *    use only the base endpoint, for example: https://westus.api.cognitive.microsoft.com
  * 
  * How to run:
  *   Run in your favorite IDE or...
  *   To use the command line:
  *      - Make sure Maven is installed: https://maven.apache.org/install.html 
- *      - Run from the ContentModerator folder: mvn compile exec:java -Dexec.cleanupDaemonThreads=false
+ *      - Run from the root folder (has pom.xml in it): mvn compile exec:java -Dexec.cleanupDaemonThreads=false
  *      - All dependencies will be automatically included. To list them (if desired): mvn dependency:list 
+ *  NOTE: You may see a "WARNING: An illegal reflective access operation has occurred..." within the results.
+ *  This does not impact a successful result, so it can be ignored.
  * 
  * Resources:
  *   - Content Moderator documentation: https://docs.microsoft.com/en-us/azure/cognitive-services/content-moderator/
@@ -39,6 +47,12 @@ public class ContentModeratorQuickstart {
 
     // TEXT MODERATION variable
     private static File textFile = new File("src\\main\\resources\\TextModeration.txt");
+    
+    // HUMAN REVIEWS - IMAGES variables
+    private static final String REVIEW_URL = "https://moderatorsampleimages.blob.core.windows.net/samples/sample5.png";
+    private static final long LATENCY_DELAY = 30; // milliseconds of delay
+    private static final String TEAM_NAME = System.getenv("CONTENT_MODERATOR_TEAM_NAME");
+    private static final String CALLBACK_ENDPOINT = System.getenv("CONTENT_MODERATOR_REVIEWS_ENDPOINT");
 
     // <snippet_evaluationdata>
     // Contains the image moderation results for an image, including text and face detection from the image.
@@ -50,10 +64,7 @@ public class ContentModeratorQuickstart {
         // The text detection results.
         public OCR TextDetection;
         // The face detection results;
-
-        
-                // Create an object in which to store the image moderation results.
-        List<EvaluationData> evaluationData = new ArrayList<EvaluationData>();public FoundFaces FaceDetection;
+        public FoundFaces FaceDetection;
     }
     // </snippet_evaluationdata>
 
@@ -68,17 +79,19 @@ public class ContentModeratorQuickstart {
         // replace the first part (for example, "westus") with your own, if needed.
         ContentModeratorClient client = 
             ContentModeratorManager.authenticate(AzureRegionBaseUrl
-                .fromString(System.getenv("CONTENT_MODERATOR_ENDPOINT")), 
-                System.getenv("CONTENT_MODERATOR_SUBSCRIPTION_KEY"));
+                                   .fromString(System.getenv("CONTENT_MODERATOR_ENDPOINT")), 
+                                        System.getenv("CONTENT_MODERATOR_SUBSCRIPTION_KEY"));
         // </snippet_client>
         
-        // Create an object in which to store the image moderation results.
+        // Create a List in which to store the image moderation results.
         List<EvaluationData> evaluationData = new ArrayList<EvaluationData>();
 
         // Moderate URL images
         moderateImages(client, evaluationData);
         // Moderate text from file
         moderateText(client);
+        // Create a human review
+        humanReviews(client);
     } 
     // End main()
 
@@ -200,6 +213,65 @@ public class ContentModeratorQuickstart {
     }
     /**
      * END - MODERATE TEXT
+     */
+    
+    /**
+     * HUMAN REVIEWS - IMAGES
+     * This example will create a review for humans to moderate on the Content Moderator website at:
+     * https://{LOCATION OR CUSTOM NAME}.contentmoderator.cognitive.microsoft.com
+     * Requires you to moderate an image on website (when prompted), 
+     * then return to the app (IDE or console) to initiate printing of the results of the review.
+     */
+    public static void humanReviews(ContentModeratorClient client) {
+        System.out.println("---------------------------------------");
+        System.out.println("HUMAN REVIEWS - IMAGES");
+        System.out.println();
+    
+        System.out.println("Creating a review for the image: " + REVIEW_URL.substring(REVIEW_URL.lastIndexOf('/')+1, REVIEW_URL.length()));
+        System.out.println();
+    
+        // Create List for the review body items
+        List<CreateReviewBodyItem> body = new ArrayList<>();
+        List<CreateReviewBodyItemMetadataItem> metadata = new ArrayList<>();
+        // This establishes the adult or racy score (sc) that will be returned (true) with the results.
+        metadata.add(new CreateReviewBodyItemMetadataItem().withKey("sc").withValue("true"));
+        body.add(new CreateReviewBodyItem().withCallbackEndpoint(CALLBACK_ENDPOINT)
+                                           .withContent(REVIEW_URL)
+                                           .withMetadata(metadata)
+                                           .withType("Image"));
+    
+        // Creates a review by POST to callback URL. Returns a list of string review IDs.
+        List<String> reviews = client.reviews().createReviews(TEAM_NAME, "Image", body, null);
+        System.out.println();
+        System.out.println("Getting review details before human review..."); // for comparison's sake
+        System.out.println();
+    
+        // Returns a Review object, notice the "reviewerResultTags" is empty.
+        Review reviewDetailsBefore = client.reviews().getReview(TEAM_NAME, reviews.get(0));
+        Gson gsonBefore = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gsonBefore.toJson(reviewDetailsBefore).toString());
+        System.out.println();
+    
+        // Go to website to perform reviews
+        System.out.println("Perform manual reviews on the Content Moderator review site, press ENTER when done...");
+        try { System.in.read(); } catch (Exception e) { e.printStackTrace(); }
+    
+        // Wait for server to process review
+        System.out.println("Wait " + LATENCY_DELAY + " seconds for the server to process the request...");
+        System.out.println();
+        try { TimeUnit.SECONDS.sleep(LATENCY_DELAY); } catch (Exception e) { e.printStackTrace(); }
+        
+    
+        // Get review details again to view changes since the human review. The "reviewerResultTags" is now full.
+        System.out.println("Getting review details after human review...");
+        System.out.println();
+        Review reviewDetailsAfter = client.reviews().getReview(TEAM_NAME, reviews.get(0));
+        Gson gsonAfter = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gsonAfter.toJson(reviewDetailsAfter).toString());
+        System.out.println();
+        }
+    /**
+     * END - HUMAN REVIEWS - IMAGES
      */
 } 
 
