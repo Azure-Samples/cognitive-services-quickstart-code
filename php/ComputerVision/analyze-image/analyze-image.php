9002267612b4fcebdc07018c0fe97d5f21bcf7bd
Patrick Farley <pafarley@microsoft.com>
2019-09-04 10:40:28 -0700

    add php and ruby comvis qss

---
+++ php/ComputerVision/analyze-image/analyze-image.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Analyze Image Sample</title>
+</head>
+<body>
+<?php
+// Replace <Subscription Key> with a valid subscription key.
+$ocpApimSubscriptionKey = '<Subscription Key>';
+
+// You must use the same location in your REST call as you used to obtain
+// your subscription keys. For example, if you obtained your subscription keys
+// from westus, replace "westcentralus" in the URL below with "westus".
+$uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/';
+
+$imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2($uriBase . '/analyze');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
+);
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'visualFeatures' => 'Categories,Description',
+    'details' => '',
+    'language' => 'en'
+);
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body parameters
+$body = json_encode(array('url' => $imageUrl));
+
+// Request body
+$request->setBody($body);
+
+try
+{
+    $response = $request->send();
+    echo "<pre>" .
+        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
+}
+catch (HttpException $ex)
+{
+    echo "<pre>" . $ex . "</pre>";
+}
+?>
+</body>
+</html>
---
+++ ruby/ComputerVision/generate-a-thumbnail/get-thumbnail.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+
+# You must use the same location in your REST call as you used to get your
+# subscription keys. For example, if you got your subscription keys from westus,
+# replace "westcentralus" in the URL below with "westus".
+uri = URI('https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/generateThumbnail')
+uri.query = URI.encode_www_form({
+    # Request parameters
+    'width' => '100',
+    'height' => '100',
+    'smartCropping' => 'true'
+})
+
+request = Net::HTTP::Post.new(uri.request_uri)
+
+# Request headers
+# Replace <Subscription Key> with your valid subscription key.
+request['Ocp-Apim-Subscription-Key'] = '<Subscription Key>'
+request['Content-Type'] = 'application/json'
+
+request.body =
+    "{\"url\": \"https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/" +
+    "Shorkie_Poo_Puppy.jpg/1280px-Shorkie_Poo_Puppy.jpg\"}"
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(request)
+end
+
+#puts response.body
