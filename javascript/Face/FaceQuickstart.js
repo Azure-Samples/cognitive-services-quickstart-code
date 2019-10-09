2017be7dd4f233d283482884a8831f1286549590
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-10-09 14:26:32 -0700

    Added Verify example

--- javascript/Face/FaceQuickstart.js
+++ javascript/Face/FaceQuickstart.js
@@ -10,16 +10,25 @@ var Face = require("@azure/cognitiveservices-face");
  * This sample includes:
  *   - Detect Faces: using a single-faced image and a group image, gets the IDs of all eligible faces.
  *   - Find Similar: using the single-faced image as a query, it searches the group image for a similar face.
- *                   It will display the confidence number (0.0 - 1.0) of any faces found.
+ *   - Verify: compares two images to see if they are of the same person. Detects their faces first.
  */
 
+ /**
+  * Shared variables
+  * These variables are shared by more than one example below.
+  */
+ const IMAGE_BASE_URL = 'https://csdx.blob.core.windows.net/resources/Face/Images/'
+
 /**
-* Authenticate
-*/
+ * AUTHENTICATE
+ */
 // Set the FACE_SUBSCRIPTION_KEY in your environment variables with your subscription key as a value.
 let credentials = new msRest.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': process.env['FACE_SUBSCRIPTION_KEY'] } });
 // Set FACE_REGION in your environment variables with its endpoint region (such as 'westus') as a value.
 let client = new Face.FaceClient(credentials, process.env['FACE_ENDPOINT']);
+/**
+ * END - AUTHENTICATE
+ */
 
 async function main() {
     /**
@@ -57,7 +66,6 @@ async function main() {
             var filteredIds = faceIds.filter((id)=> {
                 return id != null;
             })
-
             return filteredIds;
         }).catch((err) => {
             console.log(`No faces detected in group image: ${groupImageUrl}.`);
@@ -78,7 +86,7 @@ async function main() {
     // The options parameter in findSimilar(faceIds, options) 
     // is a Models.FaceFindSimilarOptionalParams parameter
     // API call returns a Promise<SimilarFace[]>.
-    client.face.findSimilar(faceId=singleDetectedFace, options={ faceIds : groupDetectedFaces })
+    await client.face.findSimilar(faceId=singleDetectedFace, options={ faceIds : groupDetectedFaces })
         .then((similars) => {
             console.log('Similar faces found in group image:');
             for (let similar of similars) {
@@ -89,8 +97,89 @@ async function main() {
                 console.log(`Confidence: ${Number(similar.confidence).toFixed(2)}.`);
             }
             console.log()
-    }).catch((err) => {
-        console.log(`No similar faces found in group image: ${groupImageUrl}.`);
-    // END - FIND SIMILAR
+        }).catch((err) => {
+            console.log(`No similar faces found in group image: ${groupImageUrl}.`);
+            throw err;
+        });
+    /**
+     * END - FIND SIMILAR
+     */ 
+
+    /**
+     * VERIFY
+     * Verify-Face-To-Face: Verify whether two faces belong to a same person or 
+     * whether one face belongs to a person. 
+     */
+    console.log("---------------------------------")
+    console.log("VERIFY")
+
+    // Create an array to hold the target photos
+    let targetImageFileNames = ['Family1-Dad1.jpg', 'Family1-Dad2.jpg']
+    // Then declare your source photos, they'll be used to query the target images
+    let sourceImageFileNames = ['Family1-Dad3.jpg', 'Family1-Son1.jpg']
+
+    console.log('Detect in source images:')
+    // Detect faces in the source image array, then get their IDs
+    let sourceFaces = await Promise.all(sourceImageFileNames.map(async (imageName) => {
+       // Returns a Promise<DetectedFace[]>
+        return client.face.detectWithUrl(IMAGE_BASE_URL + imageName)
+            .then((faces) => {
+                console.log(`${faces.length} face detected from image ${imageName} with ID ${faces[0].faceId}`)
+                let id = faces[0].faceId
+                return { id } 
+            }).catch((err) => {
+                console.log(`No face detected in: ${sourceImageFileNames[0]}.`)
+                throw err;
+            })
+    }))
+    console.log()
+
+    // Get objects out of sourceFaces array, 
+    // then get IDs and put in new array for the API call params
+    var sourceFaceIds = []
+    for (var dict of sourceFaces) {
+        sourceFaceIds.push(dict['id'])
+    }
+
+    console.log('Detect in target images:')
+    // Detect faces in the target image array, then get their IDs
+    let targetFaces = await Promise.all(targetImageFileNames.map(async (imageName) => {
+        // Returns a Promise<DetectedFace[]>
+            return client.face.detectWithUrl(IMAGE_BASE_URL + imageName)
+                .then((faces) => {
+                    console.log(`${faces.length} face detected from image ${imageName} with ID ${faces[0].faceId}`)
+                    let id = faces[0].faceId
+                    return { id } 
+                }).catch((err) => {
+                    console.log(`No face detected in: ${targetImageFileNames[0]}.`)
+                    throw err;
+                })
+        }))
+
+    // Get objects out of targetFaces array, 
+    // then get IDs and put in new array for the API call params
+    var targetFaceIds = []
+    for (var dict of targetFaces) {
+        targetFaceIds.push(dict['id'])
+    }
+    console.log()
+
+    // Compare all source images with a target image
+    // (target imnages are of same person, so we compare only 1 to both source images)
+    await Promise.all(sourceFaceIds.map(async (sourceId) => {
+        // Returns a Promise<Verify Result> 
+        return client.face.verifyFaceToFace(faceId1=sourceId, faceId2=targetFaceIds[0])
+            .then((result) => {
+                console.log(`Are image IDs ${sourceId} and ${targetFaceIds[0]} identical? ${result.isIdentical}`)
+                // Confidence score 0.0-1.0: 
+                // lower means less of a match, higher is more of a match
+                console.log(`with confidence: ${result.confidence}`)
+            }).catch((err) => {
+                throw err;
+            })
+    }))
+    /**
+     * END - VERIFY
+     */ 
 }
 main();
