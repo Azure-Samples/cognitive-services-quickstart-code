4fc7a1e142614cfee3c75647089fcf571b6bd466
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-12-05 19:42:55 -0800

    Add Maven instruction

--- java/LUIS/authoring/quickstart.java
+++ java/LUIS/authoring/quickstart.java
@@ -19,9 +19,13 @@ import java.net.*;
  * - List all applications.
  */
 
-/* To compile and run, enter the following at a command prompt:
+/* To collect the Maven libraries needed, enter this at the command prompt:
+ * mvn clean dependency:copy-dependencies
+ * 
+ * To compile and run, enter the following at a command prompt:
  * javac Quickstart.java -cp .;lib\*
  * java -cp .;lib\* Quickstart
+ * 
  * This presumes your libraries are stored in a folder named "lib"
  * directly under the current folder. If not, please adjust the
  * -classpath/-cp value accordingly.
