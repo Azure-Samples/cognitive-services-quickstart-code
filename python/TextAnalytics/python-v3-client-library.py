2337250f13bb78a293cfa21387c071bc434d23c6
Aaron Hill <aahi@microsoft.com>
2020-02-24 10:43:56 -0800

    Text analytics quickstart code

---
+++ python/TextAnalytics/python-v3-client-library.py
@@ -0,0 +1,114 @@
+from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
+
+def authenticate_client():
+    ta_credential = TextAnalyticsApiKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+def sentiment_analysis_example(client):
+
+    document = ["I had the best day of my life. I wish you were there with me."]
+    response = client.analyze_sentiment(inputs=document)[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.3f}; neutral={1:.3f}; negative={2:.3f} \n".format(
+        response.sentiment_scores.positive,
+        response.sentiment_scores.neutral,
+        response.sentiment_scores.negative,
+    ))
+    for idx, sentence in enumerate(response.sentences):
+        print("[Offset: {}, Length: {}]".format(sentence.offset, sentence.length))
+        print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+        print("Sentence score:\nPositive={0:.3f}\nNeutral={1:.3f}\nNegative={2:.3f}\n".format(
+            sentence.sentiment_scores.positive,
+            sentence.sentiment_scores.neutral,
+            sentence.sentiment_scores.negative,
+        ))
+
+            
+sentiment_analysis_example(client)
+
+def language_detection_example(client):
+    try:
+        document = ["Ce document est rédigé en Français."]
+        response = client.detect_language(inputs = document, country_hint = 'us')[0]
+        print("Language: ", response.primary_language.name)
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+language_detection_example(client)
+
+def entity_recognition_example(client):
+
+    try:
+        document = ["I had a wonderful trip to Seattle last week."]
+        result = client.recognize_entities(inputs= document)[0]
+
+        print("Named Entities:\n")
+        for entity in result.entities:
+                print("\tText: \t", entity.text, "\tCategory: \t", entity.category, "\tSubCategory: \t", entity.subcategory,
+                      "\n\tOffset: \t", entity.offset, "\tLength: \t", entity.offset, 
+                      "\tConfidence Score: \t", round(entity.score, 3), "\n")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+entity_recognition_example(client)
+
+def entity_pii_example(client):
+
+        document = ["Insurance policy for SSN on file 123-12-1234 is here by approved."]
+
+
+        result = client.recognize_pii_entities(inputs= document)[0]
+        
+        print("Personally Identifiable Information Entities: ")
+        for entity in result.entities:
+            print("\tText: ",entity.text,"\tCategory: ", entity.category,"\tSubCategory: ", entity.subcategory)
+            print("\t\tOffset: ", entity.offset, "\tLength: ", entity.length, "\tScore: {0:.3f}".format(entity.score), "\n")
+        
+entity_pii_example(client)
+
+def entity_linking_example(client):
+
+    try:
+        document = ["""Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, 
+        to develop and sell BASIC interpreters for the Altair 8800. 
+        During his career at Microsoft, Gates held the positions of chairman,
+        chief executive officer, president and chief software architect, 
+        while also being the largest individual shareholder until May 2014."""]
+        result = client.recognize_linked_entities(inputs= document)[0]
+
+        print("Linked Entities:\n")
+        for entity in result.entities:
+            print("\tName: ", entity.name, "\tId: ", entity.id, "\tUrl: ", entity.url,
+            "\n\tData Source: ", entity.data_source)
+            print("\tMatches:")
+            for match in entity.matches:
+                print("\t\tText:", match.text)
+                print("\t\tScore: {0:.3f}".format(match.score), "\tOffset: ", match.offset, 
+                      "\tLength: {}\n".format(match.length))
+            
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+entity_linking_example(client)
+
+def key_phrase_extraction_example(client):
+
+    try:
+        document = ["My cat might need to see a veterinarian."]
+
+        response = client.extract_key_phrases(inputs= document)[0]
+
+        if not response.is_error:
+            print("\tKey Phrases:")
+            for phrase in response.key_phrases:
+                print("\t\t", phrase)
+        else:
+            print(response.id, response.error)
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+        
+key_phrase_extraction_example(client)
