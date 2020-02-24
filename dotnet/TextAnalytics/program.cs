e4af4d5fa992b26e872ef55df55554e323e7f925
Aaron Hill <aahi@microsoft.com>
2020-02-24 15:13:59 -0800

    adding C# TA code

---
+++ dotnet/TextAnalytics/program.cs
@@ -0,0 +1,108 @@
+using System;
+using Azure.AI.TextAnalytics;
+
+namespace tav3quickstart
+{
+    class Program
+    {
+        private static readonly TextAnalyticsApiKeyCredential credentials = new TextAnalyticsApiKeyCredential("<replace-with-your-text-analytics-key-here>");
+        private static readonly Uri endpoint = new Uri("<replace-with-your-text-analytics-key-here>");
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            SentimentAnalysisExample(client);
+            LanguageDetectionExample(client);
+            EntityRecognitionExample(client);
+            EntityPIIExample(client);
+            EntityLinkingExample(client);
+            KeyPhraseExtractionExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void SentimentAnalysisExample(TextAnalyticsClient client)
+        {
+            string inputText = "I had the best day of my life. I wish you were there with me.";
+            DocumentSentiment documentSentiment = client.AnalyzeSentiment(inputText);
+            Console.WriteLine($"Document sentiment: {documentSentiment.Sentiment}\n");
+
+            var si = new StringInfo(inputText);
+            //var si = new System.Globalization.StringInfo(inputText);
+            foreach (var sentence in documentSentiment.Sentences)
+            {
+                Console.WriteLine($"\tSentence [offset {sentence.Offset}, length {sentence.Length}]");
+                Console.WriteLine($"\tText: \"{si.SubstringByTextElements(sentence.Offset, sentence.Length)}\"");
+                Console.WriteLine($"\tSentence sentiment: {sentence.Sentiment}");
+                Console.WriteLine($"\tPositive score: {sentence.SentimentScores.Positive:0.00}");
+                Console.WriteLine($"\tNegative score: {sentence.SentimentScores.Negative:0.00}");
+                Console.WriteLine($"\tNeutral score: {sentence.SentimentScores.Neutral:0.00}\n");
+            }
+        }
+
+        static void LanguageDetectionExample(TextAnalyticsClient client)
+        {
+            DetectedLanguage detectedLanguage = client.DetectLanguage("Ce document est rédigé en Français.");
+            Console.WriteLine("Language:");
+            Console.WriteLine($"\t{detectedLanguage.Name},\tISO-6391: {detectedLanguage.Iso6391Name}\n");
+        }
+
+        static void EntityRecognitionExample(TextAnalyticsClient client)
+        {
+            var response = client.RecognizeEntities("I had a wonderful trip to Seattle last week.");
+            Console.WriteLine("Named Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tText: {entity.Text},\tCategory: {entity.Category},\tSub-Category: {entity.SubCategory}");
+                Console.WriteLine($"\t\tOffset: {entity.Offset},\tLength: {entity.Length},\tScore: {entity.Score:F3}\n");
+            }
+        }
+
+        static void EntityPIIExample(TextAnalyticsClient client)
+        {
+            string inputText = "Insurance policy for SSN on file 123-12-1234 is here by approved.";
+            var response = client.RecognizePiiEntities(inputText);
+            Console.WriteLine("Personally Identifiable Information Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tText: {entity.Text},\tCategory: {entity.Category},\tSub-Category: {entity.SubCategory}");
+                Console.WriteLine($"\t\tOffset: {entity.Offset},\tLength: {entity.Length},\tScore: {entity.Score:F3}\n");
+            }
+        }
+
+        static void EntityLinkingExample(TextAnalyticsClient client)
+        {
+            var response = client.RecognizeLinkedEntities(
+                "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, " +
+                "to develop and sell BASIC interpreters for the Altair 8800. " +
+                "During his career at Microsoft, Gates held the positions of chairman, " +
+                "chief executive officer, president and chief software architect, " +
+                "while also being the largest individual shareholder until May 2014.");
+            Console.WriteLine("Linked Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tName: {entity.Name},\tID: {entity.Id},\tURL: {entity.Url}\tData Source: {entity.DataSource}");
+                Console.WriteLine("\tMatches:");
+                foreach (var match in entity.Matches)
+                {
+                    Console.WriteLine($"\t\tText: {match.Text}");
+                    Console.WriteLine($"\t\tOffset: {match.Offset},\tLength: {match.Length},\tScore: {match.Score:F3}\n");
+                }
+            }
+        }
+
+        static void KeyPhraseExtractionExample(TextAnalyticsClient client)
+        {
+            var response = client.ExtractKeyPhrases("My cat might need to see a veterinarian.");
+
+            // Printing key phrases
+            Console.WriteLine("Key phrases:");
+
+            foreach (string keyphrase in response.Value)
+            {
+                Console.WriteLine($"\t{keyphrase}");
+            }
+        }
+
+    }
+}
