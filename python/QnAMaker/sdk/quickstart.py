52a17e78d1d9c781724c685e6e741970f5dbe489
diberry <diberry@microsoft.com>
2020-06-18 05:55:12 -0700

    kb definition

--- python/QnAMaker/sdk/quickstart.py
+++ python/QnAMaker/sdk/quickstart.py
@@ -1,25 +1,25 @@
 # install with command:
 # py -m pip install azure-cognitiveservices-knowledge-qnamaker==0.2.0
 
+# This sample does the following tasks.
+# - Create a knowledge base.
+# - Update a knowledge base.
+# - Publish a knowledge base.
+# - Get Query runtime endpoint key
+# - Download a knowledge base.
+# - Get answer
+# - Delete a knowledge base.
+
 # <Dependencies>
 import os
 import time
 
 from azure.cognitiveservices.knowledge.qnamaker.authoring import QnAMakerClient
 from azure.cognitiveservices.knowledge.qnamaker.runtime import QnAMakerRuntimeClient
-from azure.cognitiveservices.knowledge.qnamaker.authoring.models import QnADTO, MetadataDTO, CreateKbDTO, OperationStateType, UpdateKbOperationDTO, UpdateKbOperationDTOAdd, EndpointKeysDTO
+from azure.cognitiveservices.knowledge.qnamaker.authoring.models import QnADTO, MetadataDTO, CreateKbDTO, OperationStateType, UpdateKbOperationDTO, UpdateKbOperationDTOAdd, EndpointKeysDTO, QnADTOContext
 from azure.cognitiveservices.knowledge.qnamaker.runtime.models import QueryDTO
 from msrest.authentication import CognitiveServicesCredentials
-
 # </Dependencies>
-
-# This sample does the following tasks.
-# - Create a knowledge base.
-# - Update a knowledge base.
-# - Publish a knowledge base.
-# - Download a knowledge base.
-# - Delete a knowledge base.
-# - Get published endpoint.
 
 # <Resourcevariables>
 authoring_key = 'REPLACE-WITH-YOUR-QNA-MAKER-KEY'
@@ -48,24 +48,42 @@ def _monitor_operation(client, operation):
 # <CreateKBMethod>
 def create_kb(client):
 
-    qna = QnADTO(
-        answer="You can use our REST APIs to manage your knowledge base.",
+    qna1 = QnADTO(
+        answer="Yes, You can use our [REST APIs](https://docs.microsoft.com/rest/api/cognitiveservices/qnamaker/knowledgebase) to manage your knowledge base.",
         questions=["How do I manage my knowledgebase?"],
-        metadata=[MetadataDTO(name="Category", value="api")]
+        metadata=[
+            MetadataDTO(name="Category", value="api"),
+            MetadataDTO(name="Language", value="REST"),
+        ]
     )
-    urls = ["https://docs.microsoft.com/en-in/azure/cognitive-services/qnamaker/faqs"]
+
+    qna2 = QnADTO(
+        answer="Yes, You can use our [Python SDK](https://pypi.org/project/azure-cognitiveservices-knowledge-qnamaker/) with the [Python Reference Docs](https://docs.microsoft.com/python/api/azure-cognitiveservices-knowledge-qnamaker/azure.cognitiveservices.knowledge.qnamaker?view=azure-python) to manage your knowledge base.",
+        questions=["Can I program with Python?"],
+        metadata=[
+            MetadataDTO(name="Category", value="api"),
+            MetadataDTO(name="Language", value="Python"),
+        ]
+    )
+
+    urls = []
+    files=[]
 
     create_kb_dto = CreateKbDTO(
         name="QnA Maker Python SDK Quickstart",
         qna_list=[qna],
-        urls=urls
+        urls=urls,
+        files=[],
+        enable_hierarchical_extraction=true,
+        default_answer_used_for_extraction="No answer found.",
+        language="English"
     )
     create_op = client.knowledgebase.create(create_kb_payload=create_kb_dto)
 
-    create_op = _monitor_operation(client=client, operation=create_op)
+    create_op_monitor = _monitor_operation(client=client, operation=create_op)
 
     # Get knowledge base ID from resourceLocation HTTP header
-    knowledge_base_ID = create_op.resource_location.replace("/knowledgebases/", "")
+    knowledge_base_ID = create_op_monitor.resource_location.replace("/knowledgebases/", "")
     print("Created KB with ID: {}".format(knowledge_base_ID))
 
     return knowledge_base_ID
@@ -73,12 +91,70 @@ def create_kb(client):
 
 # <UpdateKBMethod>
 def update_kb(client, kb_id):
+
+    qna3 = QnADTO(
+        answer="goodbye",
+        questions=[
+            "bye",
+            "end",
+            "stop",
+            "quit",
+            "done"
+            ],
+        metadata=[
+            MetadataDTO(name="Category", value="Chitchat"),
+            MetadataDTO(name="Chitchat", value="end"),
+        ]
+    )
+
+    qna4 = QnADTO(
+        answer="Hello, please select from the list of questions or enter a new question to continue.",
+        questions=[
+            "hello",
+            "hi",
+            "start"
+        ],
+        metadata=[
+            MetadataDTO(name="Category", value="Chitchat"),
+            MetadataDTO(name="Chitchat", value="begin"),
+        ],
+        context = QnADTOContext(
+
+            is_context_only = false,
+            prompts = [
+
+                PromptDTO(
+                    display_order =1,
+                    display_text= "Use REST",
+                    qna_id=1
+
+                ),
+                PromptDTO(
+                    display_order =2,
+                    display_text= "Use .NET NuGet package",
+                    qna_id=2
+                ),
+            }
+
+    )
+
+    urls = [
+        "https://docs.microsoft.com/azure/cognitive-services/QnAMaker/troubleshooting"
+    ]
+
+
+
     update_kb_operation_dto = UpdateKbOperationDTO(
         add=UpdateKbOperationDTOAdd(
             qna_list=[
-                QnADTO(questions=["bye"], answer="goodbye")
-            ]
-        )
+                qna3,
+                qna4
+            ],
+            urls = urls,
+            files=[]
+        ),
+        delete=None,
+        update=None
     )
     update_op = client.knowledgebase.update(kb_id=kb_id, update_kb=update_kb_operation_dto)
     _monitor_operation(client=client, operation=update_op)
