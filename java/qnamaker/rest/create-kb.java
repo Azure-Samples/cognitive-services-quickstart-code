d6a67e976254e299f8ca1675375a5adc938314d6
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2020-08-20 08:58:24 -0700

    Create create-kb.java

---
+++ java/qnamaker/rest/create-kb.java
@@ -0,0 +1,191 @@
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+// javac -cp ".;libs/*" CreateKB.java
+// java -cp ".;libs/*" CreateKB
+
+/**
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *    <dependency>
+ *      <groupId>com.google.code.gson</groupId>
+ *      <artifactId>gson</artifactId>
+ *      <version>2.8.5</version>
+ *    </dependency>
+ */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+public class CreateKB {
+
+    // Replace this with a valid subscription key.
+    static String subscriptionKey = "<your-qna-maker-subscription-key>";
+
+    // Replace this with a valid resource name.
+    static String host = "https://<your-resource-name>.api.cognitive.microsoft.com";
+    static String service = "/qnamaker/v4.0";
+    static String method = "/knowledgebases/create";
+
+    public static class KB {
+        String name;
+        Question[] qnaList;
+        String[] urls;
+        File[] files;
+    }
+
+    public static class Question {
+        Integer id;
+        String answer;
+        String source;
+        String[] questions;
+        Metadata[] metadata;
+    }
+
+    public static class Metadata {
+        String name;
+        String value;
+    }
+
+    public static class File {
+        String fileName;
+        String fileUri;
+    }
+
+    public static KB GetKB () {
+        KB kb = new KB ();
+        kb.name = "Example Knowledge Base";
+
+        Question q = new Question();
+        q.id = 0;
+        q.answer = "You can use our REST APIs to manage your Knowledge Base. See here for details: https://westus.dev.cognitive.microsoft.com/docs/services/58994a073d9e04097c7ba6fe/operations/58994a073d9e041ad42d9baa";
+        q.source = "Custom Editorial";
+        q.questions = new String[]{"How do I programmatically update my Knowledge Base?"};
+
+        Metadata md = new Metadata();
+        md.name = "category";
+        md.value = "api";
+        q.metadata = new Metadata[]{md};
+
+        kb.qnaList = new Question[]{q};
+        kb.urls = new String[]{"https://docs.microsoft.com/en-in/azure/cognitive-services/qnamaker/faqs",     "https://docs.microsoft.com/en-us/bot-framework/resources-bot-framework-faq"};
+
+        return kb;
+    }
+
+    public static String PrettyPrint (String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static class Response {
+        Map<String, List<String>> Headers;
+        String Response;
+
+        public Response(Map<String, List<String>> headers, String response) {
+            this.Headers = headers;
+            this.Response = response;
+        }
+    }
+
+    public static Response Post (URL url, String content) throws Exception{
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Content-Length", content.length() + "");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        byte[] encoded_content = content.getBytes("UTF-8");
+        wr.write(encoded_content, 0, encoded_content.length);
+        wr.flush();
+        wr.close();
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+        return new Response (connection.getHeaderFields(), response.toString());
+    }
+
+    public static Response Get (URL url) throws Exception{
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+            connection.setDoOutput(true);
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+        return new Response (connection.getHeaderFields(), response.toString());
+    }
+
+    public static Response CreateKB (KB kb) throws Exception {
+        URL url = new URL (host + service + method);
+        System.out.println ("Calling " + url.toString() + ".");
+        String content = new Gson().toJson(kb);
+        return Post(url, content);
+    }
+
+    public static Response GetStatus (String operation) throws Exception {
+        URL url = new URL (host + service + operation);
+        System.out.println ("Calling " + url.toString() + ".");
+        return Get(url);
+    }
+
+    public static void main(String[] args) {
+        try {
+            // Send the request to create the knowledge base.
+            Response response = CreateKB (GetKB ());
+
+            // Get operation ID
+            String operation = response.Headers.get("Location").get(0);
+
+            System.out.println (PrettyPrint (response.Response));
+
+            // Loop until the request is completed.
+            Boolean done = false;
+            while (!done) {
+                // Check on the status of the request.
+                response = GetStatus (operation);
+                System.out.println (PrettyPrint (response.Response));
+                
+                Type type = new TypeToken<Map<String, String>>(){}.getType();
+
+                Map<String, String> fields = new Gson().fromJson(response.Response, type);
+
+                String state = fields.get ("operationState");
+
+                // If the request is still running, the server tells us how
+                // long to wait before checking the status again.
+                if (state.equals("Running") || state.equals("NotStarted")) {
+                    String wait = response.Headers.get ("Retry-After").get(0);
+                    System.out.println ("Waiting " + wait + " seconds...");
+                    Thread.sleep (Integer.parseInt(wait) * 1000);
+                }
+                else {
+                    done = true;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println (e);
+        }
+    }
+
+}
