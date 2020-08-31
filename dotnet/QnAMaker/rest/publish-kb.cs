15d431204421790e4407abdda21d72313fa3b506
v-jawe@microsoft.com <v-jawe@microsoft.com>
2020-08-31 13:05:52 -0700

    Added include tags.

--- dotnet/QnAMaker/rest/publish-kb.cs
+++ dotnet/QnAMaker/rest/publish-kb.cs
@@ -1,10 +1,13 @@
+// <dependencies>
 using System;
 using System.Net.Http;
+// </dependencies>
 
 namespace QnAMakerPublishQuickstart
 {
     class Program
     {
+// <constants>
         private const string subscriptionKeyVar = "QNA_MAKER_SUBSCRIPTION_KEY";
         private const string endpointVar = "QNA_MAKER_ENDPOINT";
         private const string kbIdVar = "QNA_MAKER_KB_ID";
@@ -32,9 +35,11 @@ namespace QnAMakerPublishQuickstart
                 throw new Exception("Please set/export the environment variable: " + kbIdVar);
             }
         }
+// </constants>
 
         static void Main(string[] args)
         {
+// <post>
             var uri = endpoint + "/qnamaker/v4.0/knowledgebases/" + kbId;
 
             using (var client = new HttpClient())
@@ -53,6 +58,7 @@ namespace QnAMakerPublishQuickstart
                 Console.WriteLine("Press any key to continue.");
                 Console.ReadKey();
             }
+// </post>
         }
     }
 }
