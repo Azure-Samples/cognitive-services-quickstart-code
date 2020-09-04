a9114d21ff4d2d5916cc72f6c13535d80b0cbb48
Patrick Farley <pafarley@microsoft.com>
2020-09-04 15:38:46 -0700

    add scenario

--- javascript/AnomalyDetector/anomaly_detector_quickstart.js
+++ javascript/AnomalyDetector/anomaly_detector_quickstart.js
@@ -43,18 +43,18 @@ let anomalyDetectorClient = new AnomalyDetector.AnomalyDetectorClient(credential
 // </authentication>
 
 // <readFile>
-function readFile(){
+function readFile() {
     let input = fs.readFileSync(CSV_FILE).toString();
-    let parsed = parse(input, {skip_empty_lines:true});
-    parsed.forEach(function(e){
-      points.push({timestamp:new Date(e[0]), value: parseFloat(e[1])});
+    let parsed = parse(input, { skip_empty_lines: true });
+    parsed.forEach(function (e) {
+        points.push({ timestamp: new Date(e[0]), value: parseFloat(e[1]) });
     });
 }
 readFile()
 // </readFile>
 
 // <batchCall>
-async function batchCall(){
+async function batchCall() {
     // Create request body for API call
     let body = { series: points, granularity: 'daily' }
     // Make the call to detect anomalies in whole series of points
@@ -64,32 +64,59 @@ async function batchCall(){
             for (let item = 0; item < response.isAnomaly.length; item++) {
                 if (response.isAnomaly[item]) {
                     console.log("An anomaly was detected from the series, at row " + item)
-                } 
+                }
             }
         }).catch((error) => {
             console.log(error)
-        })  
-    
+        })
+
 }
 batchCall()
 // </batchCall>
 
 // <lastDetection>
-async function lastDetection(){
+async function lastDetection() {
 
     let body = { series: points, granularity: 'daily' }
-        // Make the call to detect anomalies in the latest point of a series
-        await anomalyDetectorClient.lastDetect(body)
-            .then((response) => {
-                console.log("Latest point anomaly detection:")
-                if (response.isAnomaly) {
-                    console.log("The latest point, in row " + points.length + ", is detected as an anomaly.")
-                } else {
-                    console.log("The latest point, in row " + points.length + ", is not detected as an anomaly.")
-                }
-            }).catch((error) => {
-                console.log(error)
-            })  
+    // Make the call to detect anomalies in the latest point of a series
+    await anomalyDetectorClient.lastDetect(body)
+        .then((response) => {
+            console.log("Latest point anomaly detection:")
+            if (response.isAnomaly) {
+                console.log("The latest point, in row " + points.length + ", is detected as an anomaly.")
+            } else {
+                console.log("The latest point, in row " + points.length + ", is not detected as an anomaly.")
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
 }
 lastDetection()
 // </lastDetection>
+
+// <changePointDetection>
+async function changePointDetection() {
+
+    let body = { series: points, granularity: 'daily' }
+    // get change point detect results
+    await anomalyDetectorClient.detectChangePoint(body)
+        .then((response) => {
+            if (
+                response.isChangePoint.some(function (changePoint) {
+                    return changePoint === true;
+                })
+            ) {
+                console.log("Change points were detected from the series at index:");
+                response.isChangePoint.forEach(function (changePoint, index) {
+                    if (changePoint === true) {
+                        console.log(index);
+                    }
+                });
+            } else {
+                console.log("There is no change point detected from the series.");
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+}
+// </changePointDetection>
