3aaec74e2d7403fcad3511b3e05b474ba1ceedb6
Patrick Farley <pafarley@microsoft.com>
2020-10-26 10:08:53 -0700

    updates for qs tempate

--- python/Face/FaceQuickstart.py
+++ python/Face/FaceQuickstart.py
@@ -49,13 +49,11 @@ References:
 '''
 
 # <snippet_subvars>
-# Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
 # This key will serve all examples in this document.
-KEY = os.environ['FACE_SUBSCRIPTION_KEY']
+KEY = "<your subscription key>"
 
-# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.
 # This endpoint will be used in all examples in this quickstart.
-ENDPOINT = os.environ['FACE_ENDPOINT']
+ENDPOINT = "<your api endpoint>"
 # </snippet_subvars>
 
 # <snippet_verify_baseurl>
