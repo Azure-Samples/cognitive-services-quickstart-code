a188c79e87ac66d2d027d38aa5f0d5db5e3f0752
Patrick Farley <pafarley@microsoft.com>
2020-10-25 17:17:14 -0700

    edit for qs template

--- python/ComputerVision/ComputerVisionQuickstart.py
+++ python/ComputerVision/ComputerVisionQuickstart.py
@@ -67,18 +67,9 @@ Authenticate
 Authenticates your credentials and creates a client.
 '''
 # <snippet_vars>
-# Add your Computer Vision subscription key to your environment variables.
-if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
-    subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-else:
-    print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
-# Add your Computer Vision endpoint to your environment variables.
-if 'COMPUTER_VISION_ENDPOINT' in os.environ:
-    endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-else:
-    print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-    sys.exit()
+subscription_key = "<your subscription key>"
+endpoint = "<your API endpoint>"
+
 # </snippet_vars>
 
 # <snippet_client>
