96e1dc4e64dd201ae1ae97b7d6b20952570ffb8e
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2021-01-05 18:42:42 -0800

    Update Program.cs
    Fixed multiple issues.

--- dotnet/CustomVision/ImageClassification/Program.cs
+++ dotnet/CustomVision/ImageClassification/Program.cs
@@ -1,4 +1,4 @@
-﻿// <snippet_imports>
+// <snippet_imports>
 using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
 using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
 using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
@@ -9,25 +9,40 @@ using System.Linq;
 using System.Threading;
 // </snippet_imports>
 
+/* NOTE After compiling this program, but before running it, do the following.
+ * 1. Download the contents of https://github.com/Azure-Samples/cognitive-services-sample-data-files/tree/master/CustomVision/ImageClassification. The simplest way to do this is to visit https://github.com/Azure-Samples/cognitive-services-sample-data-files and download the repo as a .zip file, then extract the contents of the CustomVision/ImageClassification folder.
+ * 2. Copy the Images folder to the output folder of your Visual Studio project. For example, if your project targets .NET Core 3.1, copy the Images folder to the /bin/Debug/netcoreapp3.1 folder.
+ */
+
 namespace ImageClassification
 {
     class Program
     {
         // <snippet_creds>
-        private const string endpoint = "<your API endpoint here>";
-        // Add your training & prediction key from the settings page of the portal
-        private const string trainingKey = "<your training key here>";
-        private const string predictionKey = "<your prediction key here>";
+        // You can obtain these values from the Keys and Endpoint page for your Custom Vision resource in the Azure Portal.
+        private static string trainingEndpoint = Environment.GetEnvironmentVariable ("CUSTOM_VISION_TRAINING_ENDPOINT");
+        private static string trainingKey = Environment.GetEnvironmentVariable("CUSTOM_VISION_TRAINING_KEY");
+        // You can obtain these values from the Keys and Endpoint page for your Custom Vision Prediction resource in the Azure Portal.
+        private static string predictionEndpoint = Environment.GetEnvironmentVariable("CUSTOM_VISION_PREDICTION_ENDPOINT");
+        private static string predictionKey = Environment.GetEnvironmentVariable ("CUSTOM_VISION_PREDICTION_KEY");
+        // You can obtain this value from the Properties page for your Custom Vision Prediction resource in the Azure Portal. See the "Resource ID" field. This typically has a value such as:
+        // /subscriptions/<your subscription ID>/resourceGroups/<your resource group>/providers/Microsoft.CognitiveServices/accounts/<your Custom Vision prediction resource name>
+        private static string predictionResourceId = Environment.GetEnvironmentVariable("CUSTOM_VISION_PREDICTION_RESOURCE_ID");
+
         private static List<string> hemlockImages;
         private static List<string> japaneseCherryImages;
+        private static Tag hemlockTag;
+        private static Tag japaneseCherryTag;
+        private static Iteration iteration;
+        private static string publishedModelName = "treeClassModel";
         private static MemoryStream testImage;
         // </snippet_creds>
 
         static void Main(string[] args)
         {
             // <snippet_maincalls>
-            CustomVisionTrainingClient TrainingApi = AuthenticateTraining(endpoint, trainingKey);
-            CustomVisionPredictionClient predictionApi = AuthenticateTraining(endpoint, predictionKey);
+            CustomVisionTrainingClient trainingApi = AuthenticateTraining(trainingEndpoint, trainingKey);
+            CustomVisionPredictionClient predictionApi = AuthenticatePrediction(predictionEndpoint, predictionKey);
 
             Project project = CreateProject(trainingApi);
             AddTags(trainingApi, project);
@@ -35,11 +50,12 @@ namespace ImageClassification
             TrainProject(trainingApi, project);
             PublishIteration(trainingApi, project);
             TestIteration(predictionApi, project);
+            DeleteProject(trainingApi, project);
             // </snippet_maincalls>
         }
 
         // <snippet_auth>
-        private CustomVisionTrainingClient AuthenticateTraining(string endpoint, string trainingKey)
+        private static CustomVisionTrainingClient AuthenticateTraining(string endpoint, string trainingKey)
         {
             // Create the Api, passing in the training key
             CustomVisionTrainingClient trainingApi = new CustomVisionTrainingClient(new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.ApiKeyServiceClientCredentials(trainingKey))
@@ -48,7 +64,7 @@ namespace ImageClassification
             };
             return trainingApi;
         }
-        private CustomVisionPredictionClient AuthenticatePrediction(string endpoint, string predictionKey)
+        private static CustomVisionPredictionClient AuthenticatePrediction(string endpoint, string predictionKey)
         {
             // Create a prediction endpoint, passing in the obtained prediction key
             CustomVisionPredictionClient predictionApi = new CustomVisionPredictionClient(new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.ApiKeyServiceClientCredentials(predictionKey))
@@ -60,25 +76,24 @@ namespace ImageClassification
         // </snippet_auth>
 
         // <snippet_create>
-        private void CreateProject(CustomVisionTrainingClient trainingApi, Project project)
+        private static Project CreateProject(CustomVisionTrainingClient trainingApi)
         {
             // Create a new project
             Console.WriteLine("Creating new project:");
-            var project = trainingApi.CreateProject("My New Project");
+            return trainingApi.CreateProject("My New Project");
         }
         // </snippet_create>
         // <snippet_addtags>
-        private void AddTags(CustomVisionTrainingClient trainingApi, Project project)
+        private static void AddTags(CustomVisionTrainingClient trainingApi, Project project)
         {
-
             // Make two tags in the new project
-            var hemlockTag = trainingApi.CreateTag(project.Id, "Hemlock");
-            var japaneseCherryTag = trainingApi.CreateTag(project.Id, "Japanese Cherry");
+            hemlockTag = trainingApi.CreateTag(project.Id, "Hemlock");
+            japaneseCherryTag = trainingApi.CreateTag(project.Id, "Japanese Cherry");
         }
         // </snippet_addtags>
 
         // <snippet_upload>
-        private void UploadImages(CustomVisionTrainingClient trainingApi, Project project)
+        private static void UploadImages(CustomVisionTrainingClient trainingApi, Project project)
         {
             // Add some images to the tags
             Console.WriteLine("\tUploading images");
@@ -101,16 +116,17 @@ namespace ImageClassification
         // </snippet_upload>
 
         // <snippet_train>
-        private void TrainProject(CustomVisionTrainingClient trainingApi, Project project)
+        private static void TrainProject(CustomVisionTrainingClient trainingApi, Project project)
         {
             // Now there are images with tags start training the project
             Console.WriteLine("\tTraining");
-            var iteration = trainingApi.TrainProject(project.Id);
+            iteration = trainingApi.TrainProject(project.Id);
 
             // The returned iteration will be in progress, and can be queried periodically to see when it has completed
             while (iteration.Status == "Training")
             {
-                Thread.Sleep(1000);
+                Console.WriteLine("Waiting 10 seconds for training to complete...");
+                Thread.Sleep(10000);
 
                 // Re-query the iteration to get it's updated status
                 iteration = trainingApi.GetIteration(project.Id, iteration.Id);
@@ -119,12 +135,8 @@ namespace ImageClassification
         // </snippet_train>
 
         // <snippet_publish>
-        private void PublishIteration(CustomVisionTrainingClient trainingApi, Project project)
+        private static void PublishIteration(CustomVisionTrainingClient trainingApi, Project project)
         {
-
-            // The iteration is now trained. Publish it to the prediction end point.
-            var publishedModelName = "treeClassModel";
-            var predictionResourceId = "<target prediction resource ID>";
             trainingApi.PublishIteration(project.Id, iteration.Id, publishedModelName, predictionResourceId);
             Console.WriteLine("Done!\n");
 
@@ -133,7 +145,7 @@ namespace ImageClassification
         // </snippet_publish>
 
         // <snippet_test>
-        private void TestIteration(CustomVisionPredictionClient predictionApi, Project project)
+        private static void TestIteration(CustomVisionPredictionClient predictionApi, Project project)
         {
 
             // Make a prediction against the new project
@@ -145,7 +157,6 @@ namespace ImageClassification
             {
                 Console.WriteLine($"\t{c.TagName}: {c.Probability:P1}");
             }
-            Console.ReadKey();
         }
         // </snippet_test>
 
@@ -158,5 +169,15 @@ namespace ImageClassification
             testImage = new MemoryStream(File.ReadAllBytes(Path.Combine("Images", "Test\\test_image.jpg")));
         }
         // </snippet_loadimages>
+        // <snippet_delete>
+        private static void DeleteProject(CustomVisionTrainingClient trainingApi, Project project)
+        {
+            // Delete project. Note you cannot delete a project with a published iteration; you must unpublish the iteration first.
+            Console.WriteLine("Unpublishing iteration.");
+            trainingApi.UnpublishIteration(project.Id, iteration.Id);
+            Console.WriteLine("Deleting project.");
+            trainingApi.DeleteProject(project.Id);
+        }
+        // </snippet_create>
     }
 }
