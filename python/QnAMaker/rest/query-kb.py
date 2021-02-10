38179f038d4c42da9024bd2199fbce2093369be8
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2021-02-09 19:37:24 -0800

    Update query-kb.py

--- python/QnAMaker/rest/query-kb.py
+++ python/QnAMaker/rest/query-kb.py
@@ -3,27 +3,30 @@ import http.client, json, os, sys
 from urllib.parse import urlparse
 # </dependencies>
 
+# Set the `authoring_key` and `authoring_endpoint` variables to your
+# QnA Maker authoring subscription key and endpoint.
+#
+# These values can be found in the Azure portal (ms.portal.azure.com/).
+# Look up your QnA Maker resource. Then, in the "Resource management"
+# section, find the "Keys and Endpoint" page.
+#
+# The value of `authoring_endpoint` has the format https://YOUR-RESOURCE-NAME.cognitiveservices.azure.com.
+#
+# Set the `runtime_endpoint` variable to your QnA Maker runtime endpoint.
+# The value of `runtime_endpoint` has the format https://YOUR-RESOURCE-NAME.azurewebsites.net.
+#
+# Set the `kb_id` variable to the ID of a knowledge base you have
+# previously created.
+
 # <constants>
-key_var_name = 'QNA_MAKER_SUBSCRIPTION_KEY'
-if not key_var_name in os.environ:
-	raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
-subscription_key = os.environ[key_var_name]
+subscription_key = 'PASTE_YOUR_QNA_MAKER_SUBSCRIPTION_KEY_HERE'
 
-authoring_endpoint_var_name = 'QNA_MAKER_ENDPOINT'
-if not authoring_endpoint_var_name in os.environ:
-	raise Exception('Please set/export the environment variable: {}'.format(authoring_endpoint_var_name))
 # Note http.client.HTTPSConnection wants only the host name, not the protocol (that is, 'https://')
-authoring_endpoint = urlparse(os.environ[authoring_endpoint_var_name]).netloc
+authoring_endpoint = urlparse('PASTE_YOUR_QNA_MAKER_ENDPOINT_HERE').netloc
 
-runtime_endpoint_var_name = 'QNA_MAKER_RUNTIME_ENDPOINT'
-if not runtime_endpoint_var_name in os.environ:
-	raise Exception('Please set/export the environment variable: {}'.format(runtime_endpoint_var_name))
-runtime_endpoint = urlparse(os.environ[runtime_endpoint_var_name]).netloc
+runtime_endpoint = urlparse('PASTE_YOUR_QNA_MAKER_RUNTIME_ENDPOINT_HERE').netloc
 
-kb_var_name = 'QNA_MAKER_KB_ID'
-if not kb_var_name in os.environ:
-	raise Exception('Please set/export the environment variable: {}'.format(kb_var_name))
-kb_id = os.environ[kb_var_name]
+kb_id = 'PASTE_YOUR_QNA_MAKER_KB_ID_HERE'
 
 get_endpoint_key_method = "/qnamaker/v4.0/endpointKeys"
 
