ef080bf043b0b8dc4f785e3d9a4aaa146e07b0d0
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2021-03-16 13:00:04 -0700

    Replace env vars for service endpoint and subscription key with string values

--- python/Face/rest/detect.py
+++ python/Face/rest/detect.py
@@ -1,16 +1,15 @@
 import json, os, requests
 
-subscription_key = os.environ['FACE_SUBSCRIPTION_KEY']
-assert subscription_key
+subscription_key = "PASTE_YOUR_FACE_SUBSCRIPTION_KEY_HERE"
 
-face_api_url = os.environ['FACE_ENDPOINT'] + '/face/v1.0/detect'
+face_api_url = "PASTE_YOUR_FACE_ENDPOINT_HERE" + '/face/v1.0/detect'
 
 image_url = 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/faces.jpg'
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
 params = {
-	'detectionModel': 'detection_02',
+    'detectionModel': 'detection_02',
     'returnFaceId': 'true'
 }
 
