583aa06fe830550d3a3d8bcdaef668c333c0411a
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2021-03-29 13:39:52 -0700

    Standardize string values for subscription keys and endpoints

--- python/FormRecognizer/FormRecognizerLogging.py
+++ python/FormRecognizer/FormRecognizerLogging.py
@@ -12,8 +12,8 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
 
-endpoint = "https://<my-custom-subdomain>.cognitiveservices.azure.com/"
-credential = AzureKeyCredential("<api_key>")
+endpoint = "PASTE_YOUR_FORM_RECOGNIZER_ENDPOINT_HERE"
+credential = AzureKeyCredential("PASTE_YOUR_FORM_RECOGNIZER_SUBSCRIPTION_KEY_HERE")
 
 # This client will log detailed information about its HTTP sessions, at DEBUG level
 form_recognizer_client = FormRecognizerClient(endpoint, credential, logging_enable=True)
