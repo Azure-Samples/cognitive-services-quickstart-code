4118375bf468ee73adb688524d59490754b9ced5
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2021-03-29 15:18:11 -0700

    Fixed variable name error.

--- python/ContentModerator/ContentModeratorQuickstart.py
+++ python/ContentModerator/ContentModeratorQuickstart.py
@@ -12,7 +12,7 @@ from msrest.authentication import CognitiveServicesCredentials
 # </snippet_imports>
 
 # <snippet_vars>
-CONTENTMODERATOR_ENDPOINT = "PASTE_YOUR_CONTENT_MODERATOR_ENDPOINT_HERE"
+CONTENT_MODERATOR_ENDPOINT = "PASTE_YOUR_CONTENT_MODERATOR_ENDPOINT_HERE"
 subscription_key = "PASTE_YOUR_CONTENT_MODERATOR_SUBSCRIPTION_KEY_HERE"
 # </snippet_vars>
 
