92855712214e0a599de6c3c6634d45f548faebf9
Patrick Farley <pafarley@microsoft.com>
2022-06-22 21:26:57 -0400

    fix python qs

--- python/Face/FaceQuickstart-single.py
+++ python/Face/FaceQuickstart-single.py
@@ -1,7 +1,6 @@
 # <snippet_single>
 import asyncio
 import io
-import glob
 import os
 import sys
 import time
@@ -34,171 +33,81 @@ PERSON_GROUP_ID = str(uuid.uuid4()) # assign a random ID (or name it anything)
 # Used for the Delete Person Group example.
 TARGET_PERSON_GROUP_ID = str(uuid.uuid4()) # assign a random ID (or name it anything)
 
-
 # Create an authenticated FaceClient.
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-
-'''
-Detect faces 
-Detect faces in two images (get ID), draw rectangle around a third image.
-'''
-print('-----------------------------')
-print()
-print('DETECT FACES')
-print()
-# Detect a face in an image that contains a single face
-single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
-single_image_name = os.path.basename(single_face_image_url)
-# We use detection model 3 to get better performance.
-detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
-if not detected_faces:
-	raise Exception('No face detected from image {}'.format(single_image_name))
-
-# Display the detected face ID in the first single-face image.
-# Face IDs are used for comparison to faces (their IDs) detected in other images.
-print('Detected face ID from', single_image_name, ':')
-for face in detected_faces: print (face.face_id)
-print()
-
-# Save this ID for use in Find Similar
-first_image_face_ID = detected_faces[0].face_id
-
-# Detect the faces in an image that contains multiple faces
-# Each detected face gets assigned a new ID
-multi_face_image_url = "http://www.historyplace.com/kennedy/president-family-portrait-closeup.jpg"
-multi_image_name = os.path.basename(multi_face_image_url)
-# We use detection model 3 to get better performance.
-detected_faces2 = face_client.face.detect_with_url(url=multi_face_image_url, detection_model='detection_03')
-
-print('Detected face IDs from', multi_image_name, ':')
-if not detected_faces2:
-	raise Exception('No face detected from image {}.'.format(multi_image_name))
-else:
-    for face in detected_faces2:
-        print(face.face_id)
-print()
-
-'''
-Print image and draw rectangles around faces
-'''
-# Detect a face in an image that contains a single face
-single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-single_image_name = os.path.basename(single_face_image_url)
-# We use detection model 3 to get better performance.
-detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
-if not detected_faces:
-	raise Exception('No face detected from image {}'.format(single_image_name))
-
-# Convert width height to a point in a rectangle
-def getRectangle(faceDictionary):
-    rect = faceDictionary.face_rectangle
-    left = rect.left
-    top = rect.top
-    right = left + rect.width
-    bottom = top + rect.height
-    
-    return ((left, top), (right, bottom))
-
-def drawFaceRectangles() :
-# Download the image from the url
-    response = requests.get(single_face_image_url)
-    img = Image.open(BytesIO(response.content))
-
-# For each face returned use the face rectangle and draw a red box.
-    print('Drawing rectangle around face... see popup for results.')
-    draw = ImageDraw.Draw(img)
-    for face in detected_faces:
-        draw.rectangle(getRectangle(face), outline='red')
-
-# Display the image in the default image browser.
-    img.show()
-
-# Uncomment this to show the face rectangles.
-#    drawFaceRectangles()
-
-print()
-'''
-END - Detect faces
-'''
-
-
-'''
-Create/Train/Detect/Identify Person Group
-This example creates a Person Group, then trains it. It can then be used to detect and identify faces in other group images.
-'''
-print('-----------------------------')
-print()
-print('PERSON GROUP OPERATIONS')
-print()
 
 '''
 Create the PersonGroup
 '''
 # Create empty Person Group. Person Group ID must be lower case, alphanumeric, and/or with '-', '_'.
 print('Person group:', PERSON_GROUP_ID)
-face_client.person_group.create(person_group_id=PERSON_GROUP_ID, name=PERSON_GROUP_ID)
+face_client.person_group.create(person_group_id=PERSON_GROUP_ID, name=PERSON_GROUP_ID, recognition_model='recognition_04')
 
 # Define woman friend
-woman = face_client.person_group_person.create(PERSON_GROUP_ID, "Woman")
+woman = face_client.person_group_person.create(PERSON_GROUP_ID, name="Woman")
 # Define man friend
-man = face_client.person_group_person.create(PERSON_GROUP_ID, "Man")
+man = face_client.person_group_person.create(PERSON_GROUP_ID, name="Man")
 # Define child friend
-child = face_client.person_group_person.create(PERSON_GROUP_ID, "Child")
+child = face_client.person_group_person.create(PERSON_GROUP_ID, name="Child")
 
 '''
-Detect faces and register to correct person
+Detect faces and register them to each person
 '''
-# Find all jpeg images of friends in working directory
-woman_images = [file for file in glob.glob('*.jpg') if file.startswith("w")]
-man_images = [file for file in glob.glob('*.jpg') if file.startswith("m")]
-child_images = [file for file in glob.glob('*.jpg') if file.startswith("ch")]
+# Find all jpeg images of friends in working directory (TBD pull from web instead)
+woman_images = ["https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Mom1.jpg", "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Mom2.jpg"]
+man_images = ["https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Dad1.jpg", "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Dad2.jpg"]
+child_images = ["https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Son1.jpg", "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Son2.jpg"]
 
-# Add to a woman person
+# Add to woman person
 for image in woman_images:
-    w = open(image, 'r+b')
 	# Check if the image is of sufficent quality for recognition.
     sufficientQuality = True
-    detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
+    detected_faces = face_client.face.detect_with_url(url=image, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
     for face in detected_faces:
         if face.face_attributes.quality_for_recognition != QualityForRecognition.high:
             sufficientQuality = False
             break
-    if not sufficientQuality: continue
-    face_client.person_group_person.add_face_from_stream(PERSON_GROUP_ID, woman.person_id, w)
+        face_client.person_group_person.add_face_from_url(PERSON_GROUP_ID, woman.person_id, image)
+        print("face {} added to person {}".format(face.face_id, woman.person_id))
 
-# Add to a man person
+    if not sufficientQuality: continue
+
+# Add to man person
 for image in man_images:
-    m = open(image, 'r+b')
 	# Check if the image is of sufficent quality for recognition.
     sufficientQuality = True
-    detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
+    detected_faces = face_client.face.detect_with_url(url=image, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
     for face in detected_faces:
         if face.face_attributes.quality_for_recognition != QualityForRecognition.high:
             sufficientQuality = False
             break
-    if not sufficientQuality: continue
-    face_client.person_group_person.add_face_from_stream(PERSON_GROUP_ID, man.person_id, m)
+        face_client.person_group_person.add_face_from_url(PERSON_GROUP_ID, man.person_id, image)
+        print("face {} added to person {}".format(face.face_id, man.person_id))
 
-# Add to a child person
+    if not sufficientQuality: continue
+
+# Add to child person
 for image in child_images:
-    ch = open(image, 'r+b')
 	# Check if the image is of sufficent quality for recognition.
     sufficientQuality = True
-    detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
+    detected_faces = face_client.face.detect_with_url(url=image, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
     for face in detected_faces:
         if face.face_attributes.quality_for_recognition != QualityForRecognition.high:
             sufficientQuality = False
+            print("{} has insufficient quality".format(face))
             break
+        face_client.person_group_person.add_face_from_url(PERSON_GROUP_ID, child.person_id, image)
+        print("face {} added to person {}".format(face.face_id, child.person_id))
     if not sufficientQuality: continue
-    face_client.person_group_person.add_face_from_stream(PERSON_GROUP_ID, child.person_id, ch)
+
 
 '''
 Train PersonGroup
 '''
-print()
-print('Training the person group...')
 # Train the person group
-face_client.person_group.train(PERSON_GROUP_ID)
+print("pg resource is {}".format(PERSON_GROUP_ID))
+rawresponse = face_client.person_group.train(PERSON_GROUP_ID, raw= True)
+print(rawresponse)
 
 while (True):
     training_status = face_client.person_group.get_training_status(PERSON_GROUP_ID)
@@ -215,16 +124,15 @@ while (True):
 Identify a face against a defined PersonGroup
 '''
 # Group image for testing against
-test_image_array = glob.glob('test-image-person-group.jpg')
-image = open(test_image_array[0], 'r+b')
+test_image = "https://csdx.blob.core.windows.net/resources/Face/Images/identification1.jpg"
 
-print('Pausing for 60 seconds to avoid triggering rate limit on free account...')
-time.sleep (60)
+print('Pausing for 10 seconds to avoid triggering rate limit on free account...')
+time.sleep (10)
 
 # Detect faces
 face_ids = []
 # We use detection model 3 to get better performance, recognition model 4 to support quality for recognition attribute.
-faces = face_client.face.detect_with_stream(image, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
+faces = face_client.face.detect_with_url(test_image, detection_model='detection_03', recognition_model='recognition_04', return_face_attributes=['qualityForRecognition'])
 for face in faces:
     # Only take the face if it is of sufficient quality.
     if face.face_attributes.quality_for_recognition == QualityForRecognition.high or face.face_attributes.quality_for_recognition == QualityForRecognition.medium:
@@ -232,21 +140,16 @@ for face in faces:
 
 # Identify faces
 results = face_client.face.identify(face_ids, PERSON_GROUP_ID)
-print('Identifying faces in {}'.format(os.path.basename(image.name)))
+print('Identifying faces in image')
 if not results:
-    print('No person identified in the person group for faces from {}.'.format(os.path.basename(image.name)))
+    print('No person identified in the person group')
 for person in results:
 	if len(person.candidates) > 0:
-		print('Person for face ID {} is identified in {} with a confidence of {}.'.format(person.face_id, os.path.basename(image.name), person.candidates[0].confidence)) # Get topmost confidence score
+		print('Person for face ID {} is identified in image, with a confidence of {}.'.format(person.face_id, person.candidates[0].confidence)) # Get topmost confidence score
 	else:
-		print('No person identified for face ID {} in {}.'.format(person.face_id, os.path.basename(image.name)))
-print()
-'''
-END - Create/Train/Detect/Identify Person Group
-'''
+		print('No person identified for face ID {} in image.'.format(person.face_id))
 
 print()
-print('-----------------------------')
 print('End of quickstart.')
 
 # </snippet_single>
