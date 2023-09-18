01d06ffe0dee02eeb40d079d972cbe06b7917600
PatrickFarley <patrickfarley.work@gmail.com>
2023-09-18 18:01:42 -0400

    update urls

--- python/ComputerVision/ComputerVisionQuickstart.py
+++ python/ComputerVision/ComputerVisionQuickstart.py
@@ -79,7 +79,7 @@ This API call can also extract handwriting style text (not shown).
 '''
 print("===== Read File - remote =====")
 # Get an image with text
-read_image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
+read_image_url = "https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"
 
 # Call API with URL and raw response (allows you to get the operation location)
 read_response = computervision_client.read(read_image_url,  raw=True)
