de57dcc63c5c5df25d213d33b022c393b3fd96c1
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-01-18 15:55:08 -0800

    Updates

--- dotnet/ComputerVision/4-0/image-analysis-quickstart/Program.cs
+++ dotnet/ComputerVision/4-0/image-analysis-quickstart/Program.cs
@@ -2,40 +2,40 @@
 // Licensed under the MIT License.
 
 // <snippet_single>
+using Azure;
+using Azure.AI.Vision.ImageAnalysis;
 using System;
-using System.IO;
-using NUnit.Framework;
 
 public class Program
 {
-    static void AnalyzeImage(){
-    string endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
-    string key = Environment.GetEnvironmentVariable("VISION_KEY");
+    static void AnalyzeImage()
+    {
+        string endpoint = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
+        string key = Environment.GetEnvironmentVariable("VISION_KEY");
 
-    ImageAnalysisClient client = new ImageAnalysisClient(new Uri(endpoint), new AzureKeyCredential(key));
+        ImageAnalysisClient client = new ImageAnalysisClient(
+            new Uri(endpoint),
+            new AzureKeyCredential(key));
 
-    List visualFeatures = [VisualFeatures.Caption, VisualFeatures.Read];
-    Uri imageURL = new Uri("https://aka.ms/azsdk/image-analysis/sample.jpg");
+        ImageAnalysisResult result = client.Analyze(
+            new Uri("https://aka.ms/azsdk/image-analysis/sample.jpg"),
+            VisualFeatures.Caption | VisualFeatures.Read,
+            new ImageAnalysisOptions { GenderNeutralCaption = true });
 
-    ImageAnalysisResult result = client.Analyze(
-        imageURL,
-        visualFeatures,
-        new ImageAnalysisOptions { GenderNeutralCaption = true });
+        Console.WriteLine("Image analysis results:");
+        Console.WriteLine(" Caption:");
+        Console.WriteLine($"   '{result.Caption.Text}', Confidence {result.Caption.Confidence:F4}");
 
-    Console.WriteLine($"Image analysis results:");
-    Console.WriteLine($" Caption:");
-    Console.WriteLine($"   '{result.Caption.Text}', Confidence {result.Caption.Confidence:F4}");
-
-    Console.WriteLine(" Read:");
-    foreach (DetectedTextBlock block in result.Read.Blocks)
-        foreach (DetectedTextLine line in block.Lines)
-        {
-            Console.WriteLine($"   Line: '{line.Text}', Bounding Polygon: [{string.Join(" ", line.BoundingPolygon)}]");
-            foreach (DetectedTextWord word in line.Words)
+        Console.WriteLine(" Read:");
+        foreach (DetectedTextBlock block in result.Read.Blocks)
+            foreach (DetectedTextLine line in block.Lines)
             {
-                Console.WriteLine($"     Word: '{word.Text}', Confidence {word.Confidence.ToString("#.####")}, Bounding Polygon: [{string.Join(" ", word.BoundingPolygon)}]");
+                Console.WriteLine($"   Line: '{line.Text}', Bounding Polygon: [{string.Join(" ", line.BoundingPolygon)}]");
+                foreach (DetectedTextWord word in line.Words)
+                {
+                    Console.WriteLine($"     Word: '{word.Text}', Confidence {word.Confidence.ToString("#.####")}, Bounding Polygon: [{string.Join(" ", word.BoundingPolygon)}]");
+                }
             }
-        }
     }
 
     static void Main()
