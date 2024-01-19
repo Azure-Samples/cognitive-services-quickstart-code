63379c04b5e4b7d79ed60d659dd5f4b583b3a317
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-01-18 17:19:46 -0800

    Update Java samples

--- java/ComputerVision/4-0/ImageAnalysisQuickStart.java
+++ java/ComputerVision/4-0/ImageAnalysisQuickStart.java
@@ -1,50 +1,26 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// DESCRIPTION:
-//     This sample demonstrates how to generate a human-readable sentence that describes the content
-//     of a publicly accessible image URL, using a synchronous client.
-//
-//     By default the caption may contain gender terms such as "man", "woman", or "boy", "girl".
-//     You have the option to request gender-neutral terms such as "person" or "child" by setting
-//     `genderNeutralCaption` to `true` when calling `analyze`, as shown in this example.
-//
-//     The synchronous (blocking) `analyze` method call returns an `ImageAnalysisResult` object.
-//     A call to `getCaption()` on this result will return a `CaptionResult` object. It contains:
-//     - The text of the caption. Captions are only supported in English at the moment. 
-//     - A confidence score in the range [0, 1], with higher values indicating greater confidences in
-//       the caption.
-//
 // USAGE:
 //     Compile the sample:
 //         mvn clean dependency:copy-dependencies
-//         javac SampleCaptionImageUrl.java -cp target\dependency\*
+//         javac ImageAnalysisQuickStart.java -cp target\dependency\*
 //     Run the sample:
-//         java -cp ".;target\dependency\*" SampleCaptionImageUrl
+//         java -cp ".;target\dependency\*" ImageAnalysisQuickStart
 //
 //     Set these two environment variables before running the sample:
 //     1) VISION_ENDPOINT - Your endpoint URL, in the form https://your-resource-name.cognitiveservices.azure.com
 //                          where `your-resource-name` is your unique Azure Computer Vision resource name.
 //     2) VISION_KEY - Your Computer Vision key (a 32-character Hexadecimal number)
 
-
 // <snippet_single>
-import com.azure.ai.vision.imageanalysis.ImageAnalysisClient;
-import com.azure.ai.vision.imageanalysis.ImageAnalysisClientBuilder;
+import com.azure.ai.vision.imageanalysis.*;
+import com.azure.ai.vision.imageanalysis.models.*;
 import com.azure.core.credential.KeyCredential;
-
-import com.azure.ai.vision.imageanalysis.models.ImageAnalysisOptions;
-import com.azure.ai.vision.imageanalysis.models.ImageAnalysisResult;
-import com.azure.ai.vision.imageanalysis.models.VisualFeatures;
-import com.azure.ai.vision.imageanalysis.models.DetectedTextLine;
-import com.azure.ai.vision.imageanalysis.models.DetectedTextWord;
-
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 
-public class ImageAnalysis {
+public class ImageAnalysisQuickStart {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
         String endpoint = System.getenv("VISION_ENDPOINT");
         String key = System.getenv("VISION_KEY");
@@ -63,9 +39,9 @@ public class ImageAnalysis {
 
         // This is a synchronous (blocking) call.
         ImageAnalysisResult result = client.analyze(
-            new URL("https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"), // imageUrl: the URL of the image to analyze
-            Arrays.asList(VisualFeatures.CAPTION, VisualFeatures.READ), // visualFeatures
-            new ImageAnalysisOptions().setGenderNeutralCaption(true)); // options:  Set to 'true' or 'false' (relevant for CAPTION or DENSE_CAPTIONS visual features)
+            new URL("https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"),
+            Arrays.asList(VisualFeatures.CAPTION, VisualFeatures.READ),
+            new ImageAnalysisOptions().setGenderNeutralCaption(true));
 
         // Print analysis results to the console
         System.out.println("Image analysis results:");
