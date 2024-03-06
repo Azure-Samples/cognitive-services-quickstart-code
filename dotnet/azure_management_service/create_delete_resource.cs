f0458c7f27326eb970896fc98b487a0b85a0d6c0
Patrick Farley <pafarley@microsoft.com>
2024-03-06 14:00:59 -0500

    prepare new code file

--- dotnet/azure_management_service/create_delete_resource.cs
+++ dotnet/azure_management_service/create_delete_resource.cs
@@ -11,42 +11,12 @@ using Microsoft.Azure.Management.CognitiveServices;
 using Microsoft.Azure.Management.CognitiveServices.Models;
 // </snippet_using>
 
-/* Note: Install the following NuGet packages:
-Microsoft.Azure.Management.CognitiveServices 
-Microsoft.Azure.Management.Fluent
-Microsoft.Azure.Management.ResourceManager.Fluent
-*/
+
 
 namespace ConsoleApp1
 {
     class Program
     {
-        /*
-        The application ID and secret of the service principal you are using to connect to the Azure Management Service.
-
-        To create a service principal with the Azure CLI, see:
-        https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli?view=azure-cli-latest
-        To install the Azure CLI, see:
-        https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
-
-        To create a service principal with Azure PowerShell, see: 
-        https://docs.microsoft.com/en-us/powershell/azure/create-azure-service-principal-azureps?view=azps-3.3.0
-        To install Azure PowerShell, see:
-        https://github.com/Azure/azure-powershell
-
-        When you create a service principal, you will see it has both an ID and an application ID.
-        For example, if you create a service principal with Azure PowerShell, it should look like the following:
-
-        Secret                : System.Security.SecureString
-        ServicePrincipalNames : {<application ID>, <application URL>}
-        ApplicationId         : <application ID>
-        ObjectType            : ServicePrincipal
-        DisplayName           : <name>
-        Id                    : <ID>
-        Type                  :
-
-        Be sure to use the service principal application ID, not simply the ID. 
-        */
 
         // <snippet_constants>
         const string  service_principal_application_id = "PASTE_YOUR_SERVICE_PRINCIPAL_APPLICATION_ID_HERE";
@@ -69,87 +39,25 @@ namespace ConsoleApp1
         const string subdomain_name = "PASTE_YOUR_SUBDOMAIN_NAME_HERE";
         // </snippet_constants>
 
-        // <snippet_list_avail>
-        static void list_available_kinds_skus_locations(CognitiveServicesManagementClient client)
-        {
-
-            Console.WriteLine("Available SKUs:");
-            var result = client.ResourceSkus.List();
-            Console.WriteLine("Kind\tSKU Name\tSKU Tier\tLocations");
-            foreach (var x in result) {
-                var locations = "";
-                foreach (var region in x.Locations)
-                {
-                    locations += region;
-                }
-                Console.WriteLine(x.Kind + "\t" + x.Name + "\t" + x.Tier + "\t" + locations);
-            };
-        }
-        // </snippet_list_avail>
-
-        // <snippet_list>
-        static void list_resources(CognitiveServicesManagementClient client)
-        {
-            Console.WriteLine("Resources in resource group: " + resource_group_name);
-            var result = client.Accounts.ListByResourceGroup(resource_group_name);
-            foreach (var x in result)
-            {
-                Console.WriteLine("ID: " + x.Id);
-                Console.WriteLine("Name: " + x.Name);
-                Console.WriteLine("Type: " + x.Type);
-                Console.WriteLine("Kind: " + x.Kind);
-                Console.WriteLine();
-            }
-        }
-        // </snippet_list>
-
-        // <snippet_create>
-        static void create_resource(CognitiveServicesManagementClient client, string resource_name, string kind, string account_tier, string location)
-        {
-            Console.WriteLine("Creating resource: " + resource_name + "...");
-            /* NOTE If you do not want to use a custom subdomain name, remove the customSubDomainName
-            property from CognitiveServicesAccountProperties. */
-            CognitiveServicesAccount parameters = 
-                new CognitiveServicesAccount(null, null, kind, location, resource_name, new CognitiveServicesAccountProperties(customSubDomainName : subdomain_name), new Sku(account_tier));
-            var result = client.Accounts.Create(resource_group_name, resource_name, parameters);
-            Console.WriteLine("Resource created.");
-            Console.WriteLine("ID: " + result.Id);
-            Console.WriteLine("Kind: " + result.Kind);
-            Console.WriteLine();
-        }
-        // </snippet_create>
-
-        // <snippet_delete>
-        static void delete_resource(CognitiveServicesManagementClient client, string resource_name)
-        {
-            Console.WriteLine("Deleting resource: " + resource_name + "...");
-            client.Accounts.Delete (resource_group_name, resource_name);
-
-            Console.WriteLine("Resource deleted.");
-            Console.WriteLine();
-        }
-        // </snippet_delete>
-
-    static void Main(string[] args)
+        static void Main(string[] args)
         {
             /* For more information about authenticating, see:
              * https://docs.microsoft.com/en-us/dotnet/azure/dotnet-sdk-azure-authenticate?view=azure-dotnet
              */
 
             // <snippet_assigns>
-            var service_principal_credentials = new ServicePrincipalLoginInformation ();
-            service_principal_credentials.ClientId = service_principal_application_id;
-            service_principal_credentials.ClientSecret = service_principal_secret;
+            // First we construct our armClient
+            ArmClient client = new ArmClient(new DefaultAzureCredential());
 
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(service_principal_application_id, service_principal_secret, tenant_id, AzureEnvironment.AzureGlobalCloud);
-            var client = new CognitiveServicesManagementClient(credentials);
-            client.SubscriptionId = subscription_id;
+            // Next we get a resource group object
+            // ResourceGroup is a [Resource] object from above
+            Subscription subscription = await client.GetDefaultSubscriptionAsync();
+            ResourceGroup resourceGroup = await subscription.GetResourceGroups().GetAsync("myRgName");
+
+
             // </snippet_assigns>
 
             // <snippet_calls>
-            // Uncomment to list all available resource kinds, SKUs, and locations for your Azure account:
-            //list_available_kinds_skus_locations(client);
-        
             // Create a resource with kind TextTranslation, F0 (free tier), location global.
             create_resource(client, "test_resource", "TextTranslation", "F0", "Global");
 
@@ -163,5 +71,33 @@ namespace ConsoleApp1
             Console.ReadKey();
             // </snippet_calls>
         }
-    }
+
+        // <snippet_create>
+        static void create_resource(CognitiveServicesManagementClient client, string resource_name, string kind, string account_tier, string location)
+        {
+
+            ResourceIdentifier id = new ResourceIdentifier("/subscriptions/{subscription_id}/resourceGroups/{resourcegroup_name}/providers/Microsoft.Network/virtualNetworks/{vnet_name}");
+
+
+            var createResult = await client.GetGenericResources().CreateOrUpdateAsync(WaitUntil.Completed, id, data);
+            
+            Console.WriteLine($"Resource {createResult.Value.Id.Name} in resource group {createResult.Value.Id.ResourceGroupName} created");
+
+        }
+        // </snippet_create>
+
+        // <snippet_list>
+        static void list_resources(CognitiveServicesManagementClient client)
+        {
+
+        }
+        // </snippet_list>
+
+        // <snippet_delete>
+        static void delete_resource(CognitiveServicesManagementClient client, string resource_name)
+        {
+
+        }
+        // </snippet_delete>
+
 }
