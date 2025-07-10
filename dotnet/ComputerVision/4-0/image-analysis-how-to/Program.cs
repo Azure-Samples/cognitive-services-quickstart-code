4acf425df2f8988b654b10c021148bf9bd6e4079
PatrickFarley <patrickfarley.work@gmail.com>
2025-07-10 13:56:39 -0400

    add imports sec

--- dotnet/ComputerVision/4-0/image-analysis-how-to/Program.cs
+++ dotnet/ComputerVision/4-0/image-analysis-how-to/Program.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// <snippet_imports>
 using Azure;
 using Azure.AI.Vision.ImageAnalysis;
 using System;
 using System.IO;
 using System.Linq;
+// </snippet_imports>
 
 public class Program
 {
